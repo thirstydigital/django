008f7adc5c05f8d04225b3b0774492e7d7e0cf3b
Tai Lee <tai.lee@3030.com.au>
2012-08-01 11:49:01 +1000

    Fixed #13252 -- Serialise with natural keys instead of primary keys.
    Add ``--natural-foreign`` and ``--natural-primary`` options, and
    deprecate the ``--natural`` option to the ``dumpdata`` management
    command.
    
    Add ``use_natural_foreign_keys`` and ``use_natural_primary_keys``
    arguments, and deprecate the ``use_natural_keys`` argument to
    ``django.core.serializers.Serializer.serialize()``.

--- django/core/serializers/python.py
+++ django/core/serializers/python.py
@@ -33,8 +33,13 @@ class Serializer(base.Serializer):
         self._current = None
 
     def get_dump_object(self, obj):
+        if not self.use_natural_primary_keys or not hasattr(obj, 'natural_key'):
+            return {
+                "pk": smart_unicode(obj._get_pk_val(), strings_only=True),
+                "model": smart_unicode(obj._meta),
+                "fields": self._current,
+            }
         return {
-            "pk": smart_text(obj._get_pk_val(), strings_only=True),
             "model": smart_text(obj._meta),
             "fields": self._current
         }
@@ -50,7 +55,7 @@ class Serializer(base.Serializer):
             self._current[field.name] = field.value_to_string(obj)
 
     def handle_fk_field(self, obj, field):
-        if self.use_natural_keys and hasattr(field.rel.to, 'natural_key'):
+        if self.use_natural_foreign_keys and hasattr(field.rel.to, 'natural_key'):
             related = getattr(obj, field.name)
             if related:
                 value = related.natural_key()
@@ -62,7 +67,7 @@ class Serializer(base.Serializer):
 
     def handle_m2m_field(self, obj, field):
         if field.rel.through._meta.auto_created:
-            if self.use_natural_keys and hasattr(field.rel.to, 'natural_key'):
+            if self.use_natural_foreign_keys and hasattr(field.rel.to, 'natural_key'):
                 m2m_value = lambda value: value.natural_key()
             else:
                 m2m_value = lambda value: smart_text(value._get_pk_val(), strings_only=True)
@@ -84,7 +89,9 @@ def Deserializer(object_list, **options):
     for d in object_list:
         # Look up the model and starting build a dict of data for it.
         Model = _get_model(d["model"])
-        data = {Model._meta.pk.attname : Model._meta.pk.to_python(d["pk"])}
+        data = {}
+        if 'pk' in d:
+            data[Model._meta.pk.attname] = Model._meta.pk.to_python(d['pk'])
         m2m_data = {}
 
         # Handle each field
@@ -129,7 +136,9 @@ def Deserializer(object_list, **options):
             else:
                 data[field.name] = field.to_python(field_value)
 
-        yield base.DeserializedObject(Model(**data), m2m_data)
+        obj = base.build_instance(Model, data, db)
+
+        yield base.DeserializedObject(obj, m2m_data)
 
 def _get_model(model_identifier):
     """
